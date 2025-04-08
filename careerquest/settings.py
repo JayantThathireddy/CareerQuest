5e90feefcbf24d311848993946833c51dd5daf98
[Gmilan21] <[Mgupta21@charlotte.edu]>
2025-04-07 21:45:18 -0400

    home page

--- careerquest/settings.py
+++ careerquest/settings.py
@@ -41,6 +41,12 @@ INSTALLED_APPS = [
     'base.apps.BaseConfig',
 ]
 
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
